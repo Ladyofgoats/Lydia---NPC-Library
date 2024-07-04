39b32afd3c5817a956c0d816adcab85b61669171
William Emery <will12emery@googlemail.com>
2024-07-04 14:48:29 +0000

    Added models.py

---
+++ NPCLibrary/NPCLibrary/models.py
@@ -0,0 +1,10 @@
+from django.db import models 
+
+Class NPC(models.Model):
+    name = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+    job = models.CharField(max_length=100)
+    description = models.TextField(max_length=300)
+    date = models.DateTimeField(auto_now_add=True)
+    #role
+    user = models.ForeignKey
