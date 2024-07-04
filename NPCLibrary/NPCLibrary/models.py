c89ac1882b020f9001aa0ea30638645ea8f0b4a6
William Emery <will12emery@googlemail.com>
2024-07-04 15:38:41 +0000

    Finishing for the day.

--- NPCLibrary/NPCLibrary/models.py
+++ NPCLibrary/NPCLibrary/models.py
@@ -4,7 +4,7 @@ Class NPC(models.Model):
     name = models.CharField(max_length=100)
     genre = models.CharField(max_length=100)
     job = models.CharField(max_length=100)
-    description = models.TextField(max_length=300)
+    description = models.TextField(max_length=400)
     date = models.DateTimeField(auto_now_add=True)
     #role
     user = models.ForeignKey
