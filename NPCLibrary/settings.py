c2fab706ca7dbcde9e33ca99c3d2ee285a09ac1c
William Emery <will12emery@googlemail.com>
2024-07-05 09:50:06 +0000

    Fixed the file layering.

--- NPCLibrary/settings.py
+++ NPCLibrary/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,6 +28,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['.gitpod.io', '.herokuapp.com', '.codeinstitute-ide.net']
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+   ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 
 # Application definition
 
@@ -37,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'NPCList'
 ]
 
 MIDDLEWARE = [
---
+++ NPCList/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class NpclistConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'NPCList'
