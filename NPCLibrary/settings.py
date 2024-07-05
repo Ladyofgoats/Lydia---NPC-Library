a607044bca5ff137e33de0d4acc0a368a05dccf3
William Emery <will12emery@googlemail.com>
2024-07-05 11:46:20 +0000

    Removed the indent.

--- NPCLibrary/settings.py
+++ NPCLibrary/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
-    if os.path.isfile("env.py"):
+if os.path.isfile("env.py"):
 import env
 
 
