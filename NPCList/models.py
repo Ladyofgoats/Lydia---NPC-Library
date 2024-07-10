f5ffd9aa73d7ad3f75aefbb06e40d08c813f78f0
William Emery <will12emery@googlemail.com>
2024-07-10 09:40:24 +0000

    Moved the css files to try to get them working and added task to the NPCList models.

--- NPCList/models.py
+++ NPCList/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
-# Create your models here.
 class NPC(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=200, unique=True)
@@ -12,10 +11,20 @@ class NPC(models.Model):
     description = models.TextField(max_length=300)
     date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(
-    User, on_delete=models.CASCADE, related_name="NPC_posts"
-)
+        User, on_delete=models.CASCADE, related_name="NPC_posts"
+    )
 
+    def __str__(self):
+        return self.name
 
-def __str__(self):
-        return self.title
+class Task(models.Model):
+    npc = models.ForeignKey(NPC, on_delete=models.CASCADE, related_name="tasks")
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=300)
+    status = models.IntegerField(choices=STATUS, default=0)
+    due_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
+
 
