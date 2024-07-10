2ae655a0acd053756b6f056c13b6c94d599611d4
William Emery <will12emery@googlemail.com>
2024-07-10 16:13:32 +0000

    Deleted old database and made a new one.

--- NPCList/admin.py
+++ NPCList/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from .models import NPC
 
+@admin.register(NPC)
+class NPCAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'genre', 'job', 'date', 'author')
+    prepopulated_fields = {'slug': ('name',)}
 
+# If you have other models like Task, register them similarly
+from .models import Task
 
-# Register your models here.
-admin.site.register(NPC)
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('name', 'npc', 'status', 'due_date')
+
